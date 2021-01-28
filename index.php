ab53753bfcb721fc3615547152dadb051ad4f91c
Alisa Llavore <allavore@mail.greenriver.edu>
2021-01-28 15:05:11 -0500

    Added alternate routes

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//Define a default root (home page)
+$f3->route('GET /', function () {
+    echo "Home Page";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Define a default root (home page)
+$f3->route('GET /order', function () {
+    echo "Order Page";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Define a default root (home page)
+$f3->route('GET /order2', function () {
+    echo "Order 2 Page";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Define a default root (home page)
+$f3->route('GET /summary', function () {
+    echo "Sumary Page";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+//Rune fat free
+$f3->run();
